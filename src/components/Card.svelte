e07dd10de6a1168119f7966a5ae459c2a00377a2
Greg Zometa <greg.so.meta@gmail.com>
2023-07-09 20:57:34 -0700

    refactor: getting ui ready before go live

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -7,19 +7,29 @@
   export let secHeading: boolean | undefined = undefined;
 
   const { title, pubDatetime, description } = frontmatter;
+  const headingClass = `
+    font-medium relative
+    text-lg text-accent xl:text-base
+    decoration-dashed active:underline xl:active:no-underline xl:no-underline
+    xl:hover:before:bottom-0 xl:hover:before:h-full
+    xl:active:before:bottom-0 xl:active:before:h-full
+    xl:before:bg-accent xl:before:w-full xl:before:h-1
+    xl:before:transition-all xl:before:ease-in-out xl:before:duration-300
+    xl:before:absolute xl:before:left-0 xl:before:bottom-1 xl:before:-z-10
+  `
 </script>
 
 <li class="my-6">
   <a
     href={href}
-    class="inline-block text-lg font-medium text-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+    class="inline-block focus-visible:no-underline focus-visible:underline-offset-0"
   >
   {#if secHeading}
-  <h2 class="text-lg font-medium decoration-dashed hover:underline">
-    {title}
-  </h2>
-    {:else}
-    <h3 class="text-lg font-medium decoration-dashed hover:underline">
+    <h2 class={headingClass}>
+      {title}
+    </h2>
+  {:else}
+    <h3 class={headingClass}>
       {title}
     </h3>
   {/if}
