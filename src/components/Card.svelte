b005ec337228ce85d59532139652577779a00817
Greg Zometa <greg.so.meta@gmail.com>
2023-07-21 21:56:39 -0700

    feat: add prettier for astro and svelte files

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -16,23 +16,20 @@
     xl:before:bg-accent xl:before:w-full xl:before:h-1
     xl:before:transition-all xl:before:ease-in-out xl:before:duration-300
     xl:before:absolute xl:before:left-0 xl:before:bottom-1 xl:before:-z-10
-  `
+  `;
 </script>
 
 <li class="my-6">
-  <a
-    href={href}
-    class="inline-block focus-visible:no-underline focus-visible:underline-offset-0"
-  >
-  {#if secHeading}
-    <h2 class={headingClass}>
-      {title}
-    </h2>
-  {:else}
-    <h3 class={headingClass}>
-      {title}
-    </h3>
-  {/if}
+  <a {href} class="inline-block focus-visible:no-underline focus-visible:underline-offset-0">
+    {#if secHeading}
+      <h2 class={headingClass}>
+        {title}
+      </h2>
+    {:else}
+      <h3 class={headingClass}>
+        {title}
+      </h3>
+    {/if}
   </a>
   <Datetime datetime={pubDatetime} />
   <p>{description}</p>
