b0658c94274f357fc759167e2ca6a8facd5815e4
Greg Zometa <138949169+greg-zometa@users.noreply.github.com>
2023-08-01 23:48:05 -0700

    docs: breakup mobile devices post (#24)

--- src/components/Datetime.svelte
+++ src/components/Datetime.svelte
@@ -15,22 +15,24 @@
   });
 </script>
 
-<div class={`flex items-center space-x-2 opacity-80 ${className}`}>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    class={`${size === "sm" ? "scale-90" : "scale-100"} inline-block h-6 w-6 fill-base`}
-    aria-hidden="true"
-  >
-    <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z" />
-    <path
-      d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"
-    />
-  </svg>
-  <span class="sr-only">Posted on:</span>
-  <span class={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-    {date}
-  </span>
-  {#if readingTime}
+<div class={`flex items-center space-x-4 opacity-80 ${className}`}>
+  <div class="flex items-center space-x-2">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class={`${size === "sm" ? "scale-90" : "scale-100"} inline-block h-6 w-6 fill-base`}
+      aria-hidden="true"
+    >
+      <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z" />
+      <path
+        d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"
+      />
+    </svg>
+    <span class="sr-only">Posted on:</span>
+    <span class={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
+      {date}
+    </span>
+  </div>
+  <div class={`flex items-center ${readingTime ? "space-x-2" : "space-x-1"}`}>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       class={`${size === "sm" ? "scale-90" : "scale-100"} inline-block h-6 w-6 fill-base`}
@@ -43,7 +45,7 @@
     </svg>
     <span class="sr-only">Reading time:</span>
     <span class={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-      {readingTime}
+      {readingTime ? readingTime : "< 1 min read"}
     </span>
-  {/if}
+  </div>
 </div>
