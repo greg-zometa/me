a7ca1ab85b860bdf888f7096cf0fd78b6bdab454
greg-zometa <greg.so.meta@gmail.com>
2023-09-16 23:58:41 -0700

    fix: image generation wip

---
+++ src/pages/[ogImage].png.ts
@@ -0,0 +1,6 @@
+import generateOgImages from "@utils/generateOgImages";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async () => new Response(await generateOgImages());
+
+export const getStaticPaths = () => [{ params: { ogImage: "generating images with " } }];
