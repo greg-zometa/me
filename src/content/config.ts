3c87b9e8abd1cb97d1821eaa58f00e9302e0dce1
Greg Zometa <greg.so.meta@gmail.com>
2023-10-06 23:53:55 -0700

    refactor: separate content directories

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,27 +1,34 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z, type ImageFunction } from "astro:content";
 import { SITE } from "@config";
+
+const postSchema = (image: ImageFunction) =>
+  z.object({
+    author: z.string().default(SITE.author),
+    pubDatetime: z.date(),
+    title: z.string(),
+    readingTime: z.string().optional(),
+    postSlug: z.string().optional(),
+    featured: z.boolean().optional(),
+    draft: z.boolean().optional(),
+    tags: z.array(z.string()).default(["others"]),
+    ogImage: image()
+      .refine((img) => img.width >= 1200 && img.height >= 630, {
+        message: "OpenGraph image must be at least 1200 X 630 pixels!"
+      })
+      .or(z.string())
+      .optional(),
+    description: z.string(),
+    canonicalURL: z.string().optional()
+  });
 
 const blog = defineCollection({
   type: "content",
-  schema: ({ image }) =>
-    z.object({
-      author: z.string().default(SITE.author),
-      pubDatetime: z.date(),
-      title: z.string(),
-      readingTime: z.string().optional(),
-      postSlug: z.string().optional(),
-      featured: z.boolean().optional(),
-      draft: z.boolean().optional(),
-      tags: z.array(z.string()).default(["others"]),
-      ogImage: image()
-        .refine((img) => img.width >= 1200 && img.height >= 630, {
-          message: "OpenGraph image must be at least 1200 X 630 pixels!"
-        })
-        .or(z.string())
-        .optional(),
-      description: z.string(),
-      canonicalURL: z.string().optional()
-    })
+  schema: ({ image }) => postSchema(image)
 });
 
-export const collections = { blog };
+const comptia = defineCollection({
+  type: "content",
+  schema: ({ image }) => postSchema(image)
+});
+
+export const collections = { blog, comptia };
